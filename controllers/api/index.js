e8326c1b82d59ea91b97ae93b6d8cfef11c93a61
TurboTeam335 <105672233+dgmascal@users.noreply.github.com>
2023-05-25 20:57:52 -0500

    server issue resolved

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const searchRoutes = require('./searchRoutes');
+const searchRoutes = require('./searchRouts');
 
 router.use('/users', userRoutes);
 router.use('/search', searchRoutes);
